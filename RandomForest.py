2d1e25c1a0a5b887c5ef600a8a1a0af8eab9d1b3
anil884 <anil884@gmail.com>
2015-11-01 15:57:44 -0500

    Classifier update with K fold

--- RandomForest.py
+++ RandomForest.py
@@ -21,20 +21,36 @@ class RandomForest:
 
 	__slots__ = ('forest')
 
-	def train( self, observations ):
-                self.forest = SklearnClassifier(SVC())
+	def train( self, observations , k=5 ):
+		self.forest = []
+		splitdata = np.array_split(observations, k)
+		combos = list(reversed(list(itertools.combinations(splitdata, k-1))))
+		accuracy_sum = 0
+		for i in range(k):
+			train = list(itertools.chain(*combos[i]))
+			test = splitdata[i]
+			c = SklearnClassifier(SVC())
+			c.train(train)
+			accuracy_sum += nltk.classify.accuracy(c,test)
+			self.forest.append(c)
+
+		print('Accuracy on Train data(Using K fold)= ', accuracy_sum/k )
+		#self.forest = SklearnClassifier(SVC())
                 #self.forest = nltk.NaiveBayesClassifier.train(observations)
                 #self.forest.show_most_informative_features()
-                self.forest.train(observations)
+                #self.forest.train(observations)
 
 	def predict( self, observations, groundTruth = False):
 		if groundTruth:
-			predict = np.array([[self.forest.classify(fe),g] for (fe,g) in obesravations])
+			predict = np.array([[self.classify(fe),g] for (fe,g) in obesravations])
 		else:
-			predict = np.array([[self.forest.classify(fe)] for fe  in obesravations])
+			predict = np.array([[self.classify(fe)] for fe  in obesravations])
 
 		return predict
+	
+	def classify( self, observation):
+		return nltk.FreqDist([c.classify(fe)  for c in self.forest]).max()
 
 	def accuracy( self, observations ):
-		return nltk.classify.accuracy(self.forest,observations)
+		return sum([nltk.classify.accuracy(c,observations) for c in self.forest])/len(self.forest)
 
