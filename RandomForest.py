512a61b22e882c3ed95bb4f6bec8046a5e1e6fc1
anil884 <anil884@gmail.com>
2015-11-01 16:44:30 -0500

    Classifier update

--- RandomForest.py
+++ RandomForest.py
@@ -22,7 +22,7 @@ class RandomForest:
 
 	__slots__ = ('forest')
 
-	def train( self, observations , k=5 ):
+	def train( self, observations , cls = SVC() , k=5 ):
 		self.forest = []
 		splitdata = np.array_split(observations, k)
 		combos = list(reversed(list(itertools.combinations(splitdata, k-1))))
@@ -30,7 +30,7 @@ class RandomForest:
 		for i in range(k):
 			train = list(itertools.chain(*combos[i]))
 			test = splitdata[i]
-			c = SklearnClassifier(SVC())
+			c = SklearnClassifier(cls)
 			c.train(train)
 			accuracy_sum += nltk.classify.accuracy(c,test)
 			self.forest.append(c)
@@ -43,15 +43,20 @@ class RandomForest:
 
 	def predict( self, observations, groundTruth = False):
 		if groundTruth:
-			predict = np.array([[self.classify(fe),g] for (fe,g) in obesravations])
+			predict = np.array([[self.classify(fe),g] for fe,g in observations])
 		else:
-			predict = np.array([[self.classify(fe)] for fe  in obesravations])
+			predict = np.array([[self.classify(fe)] for fe  in observations])
 
 		return predict
 	
-	def classify( self, observation):
+	def classify( self, fe):
 		return nltk.FreqDist([c.classify(fe)  for c in self.forest]).max()
 
 	def accuracy( self, observations ):
 		return sum([nltk.classify.accuracy(c,observations) for c in self.forest])/len(self.forest)
+
+	def cm( self, data ):
+		output= np.array(self.predict(data,True))
+		print(nltk.ConfusionMatrix(list(output[:,1]),list(output[:,0])))
+
 
